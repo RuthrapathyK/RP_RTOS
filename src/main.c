086740bb9f82170f144777846ac9e9ef4c35a13c
Ruthrapathy K <kruthrapathy@gmail.com>
2024-02-11 16:18:00 +0530

    External_Pin_Interrupt_Polling

--- src/main.c
+++ src/main.c
@@ -11,24 +11,24 @@ void main()
   LED_Init(LED_RED);
   LED_Init(LED_BLUE);
   LED_Init(LED_GREEN);
+
+  //Select GPIO as Alternate Functions
+  GPIOF->AFSEL &= ~(1<<4);
+
+  // Pull up the pin
+  GPIOF->PUR |= (1<<4);
+
+  // Enable Digital I/O
+  GPIOF->DEN |= (1<<4);
+
   
   while(1)
   {
-    //Toggle the LEDs
-    LED_RED_ON;
-     delay(1000);
-    LED_RED_OFF;
-    delay(1000);
+    while((GPIOF->DATA_Bits[1<<4]))
+    ;
+
+    GPIOF->DATA_Bits[2] ^= 0xFF;
     
-    LED_BLUE_ON;
-    delay(1000);
-    LED_BLUE_OFF;
-    delay(1000);
-    
-    LED_GREEN_ON;
-    delay(1000);
-    LED_GREEN_OFF;
-    delay(1000);
   }
 }
 
