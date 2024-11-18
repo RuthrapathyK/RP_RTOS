ff6f671e9e0ea4046c6e94955d982f6f1237309e
Ruthrapathy K <kruthrapathy@gmail.com>
2024-11-18 16:40:10 +0530

    Red LED not blinking issue solved but still Stck is growing

--- src/main.c
+++ src/main.c
@@ -5,19 +5,23 @@
 uint8_t task_switch = 0;
 uint32_t * sp = 0x0;
 
-void Led_On(void)
+void Task_1(void)
 {
-  while(1)
+  while(1){
     LED_RED_ON;
+    delayTimer(100);
+    LED_RED_OFF;
+    delayTimer(100);
+  }
 }
 
-void Led_Off(void)
+void Task_2(void)
 {
   while(1){
     LED_BLUE_ON;
-    delayTimer(100);
+    delayTimer(500);
     LED_BLUE_OFF;
-    delayTimer(100);
+    delayTimer(500);
   }
 }
 void SysTick_handler(void)
@@ -27,31 +31,27 @@ void SysTick_handler(void)
 
   task_switch = !task_switch;
 
-   if(task_switch){
-    __asm("add	r9, sp, #0x18"); // Locate the LR in Stack frame and store that address in R9
-    __asm("MOV r10, #0x26c"); // Load the Led On function address in  R10
-    __asm("STR r10,[r9]"); // Store the Led On function address in LR of stack frame
+   if(task_switch)
+   {
+      __asm("add	r9, sp, #0x18"); // Locate the LR in Stack frame and store that address in R9
+      __asm("MOV r10, #0x26c"); // Load the Led On function address in  R10
+      __asm("STR r10,[r9]"); // Store the Led On function address in LR of stack frame
 
-    __asm("add r9, #4");
-    __asm("SUB r10, #1"); // Load the Led On function address in  R10
-    __asm("STR r10,[r9]"); // Store the Led On function address in PC of stack frame
+      __asm("add r9, #4");
+      __asm("SUB r10, #1"); // Load the Led On function address in  R10
+      __asm("STR r10,[r9]"); // Store the Led On function address in PC of stack frame
+   }
 
-     //*(uint32_t *)0x20000FDC = &Led_On; // Load LR register
-     //*(uint32_t *)0x20000FE0 = &Led_On - 1; // Load PC register
-     }
+   else
+   {
+      __asm("add	r9, sp, #0x18"); // Locate the LR in Stack frame and store that address in R9
+      __asm("MOV r10, #0x290"); // Load the Led On function address in  R10
+      __asm("STR r10,[r9]"); // Store the Led On function address in LR of stack frame
 
-   else{
-    __asm("add	r9, sp, #0x18"); // Locate the LR in Stack frame and store that address in R9
-    __asm("MOV r10, #0x27c"); // Load the Led On function address in  R10
-    __asm("STR r10,[r9]"); // Store the Led On function address in LR of stack frame
-
-    __asm("add r9, #4");
-    __asm("SUB r10, #1"); // Load the Led On function address in  R10
-    __asm("STR r10,[r9]"); // Store the Led On function address in PC of stack frame
-
-     //*(uint32_t *)0x20000FDC = &Led_Off; // Load LR register
-     //*(uint32_t *)0x20000FE0 = &Led_Off - 1; // Load PC register
-     }      
+      __asm("add r9, #4");
+      __asm("SUB r10, #1"); // Load the Led On function address in  R10
+      __asm("STR r10,[r9]"); // Store the Led On function address in PC of stack frame   
+    }      
 }
 
 void main()
@@ -64,7 +64,10 @@ void main()
 
   scheduler_Init(1000000);
 
-  while(1);
+  while(1)
+  {
+    //Led_Off();
+  }
 
 }
 
